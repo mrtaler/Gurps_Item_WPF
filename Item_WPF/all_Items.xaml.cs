d9640338f721befaff44f430268ce142443b7779
mrtaler <mrtaler@me.com>
2016-01-19 21:18:50 +0300

    wiew all items is new

--- Item_WPF/all_Items.xaml.cs
+++ Item_WPF/all_Items.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Data;
 using System.Linq;
 using System.Text;
@@ -21,157 +23,108 @@ namespace Item_WPF
     /// </summary>
     public partial class all_Items : Window
     {
+        private item1Entities context;
+
+
+        private ObservableCollection<ITEM> _items { get; set; }
+        private ObservableCollection<ItemClass> _itemsClass { get; set; }
         public all_Items()
         {
             InitializeComponent();
-            using (item1Entities context = new item1Entities())
-            {
-                //Load in to sort list
-                ItemClass_forSort.ItemsSource = context.ItemClasses.ToList();
-                ItemClass_forSort.DisplayMemberPath = "name";
-                ItemClass_forSort.SelectedValuePath = "id";
-                //ItemClass_forSort.SelectedValue = 2;
-                //Load in to main table////////////
-                //List<ITEM> item = (from p in context.ITEMs
-                //                   where p.usItemClass ==18
-                //                   select p).ToList();
-                //dataGridViewAllItems.ItemsSource = item.Select(s => new
-                dataGridViewAllItems.ItemsSource = context.ITEMs.Select(s => new
-                {
-                    s.uiIndex,
-                    s.szItemName,
-                    s.ItemClass.name,
-                    s.TL1.name_TL,
-                    s.LC1.Name_LC,
-                    s.usPrice
-                }).ToList(); // шрузим таблицу итем вместе с подчненной записью
-                dataGridViewAllItems.SelectedValuePath = "uiIndex";
+            context = new item1Entities();
+            _items = new ObservableCollection<ITEM>(context.ITEMs);
+            _itemsClass = new ObservableCollection<ItemClass>(context.ItemClasses);
+            ItemClass_forSort.ItemsSource = _itemsClass;
+            dataGridViewAllItems.ItemsSource = _items;
+            _items.CollectionChanged += new NotifyCollectionChangedEventHandler(_items_CollectionChanged);
 
-            }
         }
-
+        private void _items_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            dataGridViewAllItems.Items.Refresh();
+        }
+        private void ItemClass_forSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ItemClass_forSort.SelectedValue != null)
+            {
+                int selclass = Convert.ToInt32(ItemClass_forSort.SelectedValue);
+                dataGridViewAllItems.ItemsSource = _items.Where(p => p.usItemClass == selclass);
+            }
+            else dataGridViewAllItems.ItemsSource = _items;
+        }
+        /// <summary>
+        /// sort_refresh
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button4_Click(object sender, RoutedEventArgs e)//sort by class
         {
-            using (item1Entities context = new item1Entities())
-            {
-                if (ItemClass_forSort.SelectedValue != null)
-                {
-                    int selclass = Convert.ToInt32(ItemClass_forSort.SelectedValue);
-                    List<ITEM> item = (from p in context.ITEMs
-                                       where p.usItemClass == selclass
-                                       select p).ToList();
-                    dataGridViewAllItems.ItemsSource = item.Select(s => new
-                    {
-                        s.uiIndex,
-                        s.szItemName,
-                        s.ItemClass.name,
-                        s.TL1.name_TL,
-                        s.LC1.Name_LC,
-                        s.usPrice
-                    }).ToList(); // шрузим таблицу итем вместе с подчненной записью
-                    dataGridViewAllItems.SelectedValuePath = "uiIndex";
-                }
-                else
-                {
-                    dataGridViewAllItems.ItemsSource = context.ITEMs.Select(s => new
-                    {
-                        s.uiIndex,
-                        s.szItemName,
-                        s.ItemClass.name,
-                        s.TL1.name_TL,
-                        s.LC1.Name_LC,
-                        s.usPrice
-                    }).ToList(); // шрузим таблицу итем вместе с подчненной записью
-                    dataGridViewAllItems.SelectedValuePath = "uiIndex";
-                }
-            }
-        }
-
-
-        private void button3_Click(object sender, RoutedEventArgs e)//refresh
-        {
-            using (item1Entities context = new item1Entities())
-            {
-                dataGridViewAllItems.ItemsSource = context.ITEMs.Select(s => new
-                {
-                    s.uiIndex,
-                    s.szItemName,
-                    s.ItemClass.name,
-                    s.TL1.name_TL,
-                    s.LC1.Name_LC,
-                    s.usPrice
-                }).ToList();
-                dataGridViewAllItems.SelectedValuePath = "uiIndex";
-            }
+            dataGridViewAllItems.ItemsSource = _items;
+            ItemClass_forSort.SelectedIndex = -1;
+            dataGridViewAllItems.Items.Refresh();
         }
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
             if (dataGridViewAllItems.SelectedItem != null)
             {
-                using (item1Entities context = new item1Entities())
+
+                datachange.ID_sel = Convert.ToInt32(dataGridViewAllItems.SelectedValue);
+
+                ITEM itt = (from p in _items
+                            where p.uiIndex == datachange.ID_sel
+                            select p).First();
+                datachange.Class_Cha = (from p in _items
+                                        where p.uiIndex == datachange.ID_sel
+                                        select p.ItemClass.name).First();
+                datachange.ID_change = (from p in _items
+                                        where p.uiIndex == datachange.ID_sel
+                                        select p.ubClassIndex).First(); //передача ИД записи изменяемой таблицы
+
+                if (itt.used == false)// созаем сущьность для сравнения
                 {
-                    datachange.ID_sel = Convert.ToInt32(dataGridViewAllItems.SelectedValue);
-                    ITEM itt = (from p in context.ITEMs
-                                where p.uiIndex == datachange.ID_sel
-                                select p).First();
-                    datachange.Class_Cha = (from p in context.ITEMs
-                                            where p.uiIndex == datachange.ID_sel
-                                            select p.ItemClass.name).First();
-                    datachange.ID_change = (from p in context.ITEMs
-                                            where p.uiIndex == datachange.ID_sel
-                                            select p.ubClassIndex).First(); //передача ИД записи изменяемой таблицы
-
-                    if (itt.used == false)// созаем сущьность для сравнения
+                    if (datachange.Class_Cha == "Gun"
+                     || datachange.Class_Cha == "Knife"
+                     || datachange.Class_Cha == "Throwing Knife"
+                     || datachange.Class_Cha == "Launcher"
+                     || datachange.Class_Cha == "Thrown Weapon"
+                     || datachange.Class_Cha == "Blunt Weapon")
                     {
-                        if (datachange.Class_Cha == "Gun"
-                         || datachange.Class_Cha == "Knife"
-                         || datachange.Class_Cha == "Throwing Knife"
-                         || datachange.Class_Cha == "Launcher"
-                         || datachange.Class_Cha == "Thrown Weapon"
-                         || datachange.Class_Cha == "Blunt Weapon")
-                        {
-                            label01.Content = "Selected TABLE IS: WEAPON";
-                            label02.Content = "ch ID main:" + datachange.ID_sel.ToString();
-                            label03.Content = "ch ID target:" + datachange.ID_change.ToString();
+                        label01.Content = "Selected TABLE IS: WEAPON";
+                        label02.Content = "ch ID main:" + datachange.ID_sel.ToString();
+                        label03.Content = "ch ID target:" + datachange.ID_change.ToString();
 
-                            itt.used = true;
-                            itt.dt = System.DateTime.UtcNow;
-                            context.SaveChanges();
+                        itt.used = true;
+                        itt.dt = System.DateTime.UtcNow;
+                        context.SaveChanges();
 
-                            Weapon_N_R WeaponNR = new Weapon_N_R();
-                            WeaponNR.ShowDialog();
-                        }
-                        else if (itt.ItemClass.name == "Attachment")
-                        {
-                            label01.Content = "Selected TABLE IS:Attacment";
-                            label02.Content = "ch ID main:" + datachange.ID_sel.ToString();
-                            label03.Content = "ch ID target:" + datachange.ID_change.ToString();
-
-                            itt.used = true;
-                            itt.dt = System.DateTime.UtcNow;
-                            context.SaveChanges();
-                            Attach_N_R AttachNR = new Attach_N_R();
-                            AttachNR.ShowDialog();
-                        }
-
+                        Weapon_N_R WeaponNR = new Weapon_N_R();
+                        WeaponNR.ShowDialog();
                     }
-                    else MessageBox.Show("no");
-                    //          context.Database.Connection.Close();
+                    else if (itt.ItemClass.name == "Attachment")
+                    {
+                        label01.Content = "Selected TABLE IS:Attacment";
+                        label02.Content = "ch ID main:" + datachange.ID_sel.ToString();
+                        label03.Content = "ch ID target:" + datachange.ID_change.ToString();
+
+                        itt.used = true;
+                        itt.dt = System.DateTime.UtcNow;
+                        context.SaveChanges();
+                        Attach_N_R AttachNR = new Attach_N_R();
+                        AttachNR.ShowDialog();
+                    }
                 }
-            }
-        }
+                else MessageBox.Show("no");
+                            }
+                    }
 
         private void button2_Click(object sender, RoutedEventArgs e)
         {
-
-            using (item1Entities context = new item1Entities())
-            {
                 if (dataGridViewAllItems.SelectedValue != null)
                 {
 
                     int ID_sel = Convert.ToInt32(dataGridViewAllItems.SelectedValue);
-                    ITEM item = (from p in context.ITEMs
+                    ITEM item = (from p in _items
                                  where p.uiIndex == ID_sel
                                  select p).First();
                     if (item.used == false)
@@ -221,8 +174,7 @@ namespace Item_WPF
                     }
                     else MessageBox.Show("This rows is used");
                 }
-            }
-        }
+                    }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
@@ -232,42 +184,16 @@ namespace Item_WPF
 
         private void dataGridViewAllItems_LoadingRow(object sender, DataGridRowEventArgs e)
         {
-            //bool ew2;
-            //ITEM item1 = (ITEM)e.Row.DataContext;
-            ////ITEM item1 = e.Row.Item as ITEM;
-            //if (item1 != null)
-            //{
-            //    ew2 = item1.used;
-            //    if (ew2) e.Row.Background = System.Windows.Media.Brushes.LightGreen;
-            //    else e.Row.Background = System.Windows.Media.Brushes.White;
-            //}
+            bool ew2;
+            //ITEM item1 = (ITEM)e.Row.Item;
+            ITEM item1 = e.Row.DataContext as ITEM;
+            if (item1 != null)
+            {
+                ew2 = item1.used;
+                if (ew2) e.Row.Background = System.Windows.Media.Brushes.LightGreen;
+                else e.Row.Background = System.Windows.Media.Brushes.White;
+            }
         }
-
-
-        //    else if (itt.ItemClass.name == "Grenade" || itt.ItemClass.name == "Bomb")
-        //    {
-        //        label2.Text = "Selected TABLE IS:EXPLOSIVE";
-        //    }// использование таблицы EXPLOSIVE для создания новой записи
-        //    //   else if (Tex1 == "Ammo") label6.Text = "Selected TABLE IS:MAGAZINE";
-        //    else if (itt.ItemClass.name == "Armour")
-        //    {
-        //        label2.Text = "Selected TABLE IS:ARMOUR";
-        //    }
-        //    else if (itt.ItemClass.name == "Load Bearing Equipment")
-        //    {
-        //        label2.Text = "Selected TABLE IS:LBE";
-        //    }
-        //    else if (itt.ItemClass.name == "Attachment")
-        //    {
-        //        label2.Text = "Deleted TABLE IS:Attacment";
-
-        //    }
-        //    else
-        //    {
-        //        label1.Text = "del item ID:" + ID_sel.ToString();
-        //    }
-        //    context.Database.Connection.Close();
-        //}
-    }
+            }
 }
 
