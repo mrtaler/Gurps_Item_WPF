aa4e4f4c97c09a6fae00bddceca5422af08a39b3
Taler <mrtaler@me.com>
2016-04-05 15:54:46 +0300

    up

---
+++ Item_WPF/MVVM/View/EditPrimaryStatsWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using GurpsCharacterEditor.ViewModels;
+
+namespace GurpsCharacterEditor.Views
+{
+    public partial class EditPrimaryStatsWindow : Window
+    {
+        public EditPrimaryStatsWindow()
+        {
+            DataContext = new EditPrimaryStatsViewModel();
+            InitializeComponent();
+        }
+
+        private void OkButtonClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void CancelButtonClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
