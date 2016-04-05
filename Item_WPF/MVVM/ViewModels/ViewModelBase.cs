7c06f47fc578b0809eadaf2a45ee51fd97c414ea
Taler <mrtaler@me.com>
2016-04-05 15:44:26 +0300

    From clausjensbymadsen/gurpscharactereditor

---
+++ Item_WPF/MVVM/ViewModels/ViewModelBase.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace GurpsCharacterEditor.ViewModels
+{
+    // Base class for the viewmodels, which implements the INotifyPropertyChanged interface.
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Dictionary which holds information about which property is dependent on which. This is used by the
+        // property change notification system.
+        //
+        // Key: a property of the viewmodel.
+        // Value: an array of properties, which are dependent on the property.
+        protected Dictionary<string, string[]> PropertyDependencyMap = new Dictionary<string, string[]>();
+
+        // Notify registered listeners that a change has happened. Used when setting property values.
+        protected void NotifyPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                // Notify property change
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+
+                // Notify dependencies, if any
+                if (PropertyDependencyMap.ContainsKey(property))
+                    foreach (string dependencyProperty in PropertyDependencyMap[property])
+                        NotifyPropertyChanged(dependencyProperty);
+            }
+        }
+    }
+}
