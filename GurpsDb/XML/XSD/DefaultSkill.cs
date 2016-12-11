944335b7c98e8aea23ca3743fe2219cd1d90fe57
Taler <mrtaler@me.com>
2016-12-11 21:58:50 +0300

    up

---
+++ GurpsDb/XML/XSD/DefaultSkill.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GurpsDb.XML.XSD
+{
+    public class DefaultSkill
+    {
+        public string nameMainSkill;
+        public string nameSubSkill;
+        public string specialization;
+        public string type;
+        public string modifier;
+    }
+}
